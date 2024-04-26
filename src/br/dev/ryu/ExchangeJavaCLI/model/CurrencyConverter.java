63f43bd50f224223e34bfa09e8a39386e5a90bc9
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-04-26 17:33:43 -0300

    feat: Adiciona função getQuota

--- src/br/dev/ryu/ExchangeJavaCLI/model/CurrencyConverter.java
+++ src/br/dev/ryu/ExchangeJavaCLI/model/CurrencyConverter.java
@@ -6,4 +6,5 @@ import java.util.HashMap;
 public interface CurrencyConverter {
     double convert(double amount, Currency fromCurrency, Currency toCurrency) throws IOException, InterruptedException;
     HashMap<String, Currency> getCurrencies() throws IOException, InterruptedException;
+    ExchangeRateQuota getQuota() throws IOException, InterruptedException;
 }
