8d947f4c17563d870f1dd1f2fa2cfe34e0dbacaf
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-04-26 01:16:38 -0300

    refactor: Renomeia atributo formalName para name

--- src/br/dev/ryu/ExchangeJavaCLI/model/Currency.java
+++ src/br/dev/ryu/ExchangeJavaCLI/model/Currency.java
@@ -1,8 +1,8 @@
 package br.dev.ryu.ExchangeJavaCLI.model;
 
-public record Currency(String code, String formalName) {
+public record Currency(String code, String name) {
     @Override
     public String toString() {
-        return String.format("%s (%s) ", this.formalName, this.code);
+        return String.format("%s (%s) ", this.name, this.code);
     }
 }
